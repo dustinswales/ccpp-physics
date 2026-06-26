853b37b8f7ce856143c3803be5f7c6072262c84e
Dustin Swales <dustin.swales@noaa.gov>
2026-06-26 22:58:18 +0000

    Initial commit

---
+++ physics/Interstitials/MPAS/MPAS_setup.F90
@@ -0,0 +1,30 @@
+!> \file MPAS_setup.F90
+!! This file initializes
+
+module MPAS_setup
+  use mpi_f08
+  use machine, only : kind_phys
+
+  implicit none
+
+  public MPAS_setup_init
+
+  private
+
+contains
+
+!> \section arg_table_MPAS_setup_init Argument Table
+!! \htmlinclude GFS_rrtmgp_setup_init.html
+!!
+  subroutine MPAS_setup_init(errmsg, errflg)
+    ! Outputs
+    character(len=*), intent(out)   :: errmsg
+    integer,          intent(out)   :: errflg
+
+    ! Initialize the CCPP error handling variables
+    errmsg = ''
+    errflg = 0
+
+  end subroutine MPAS_setup_init
+
+end module MPAS_setup
