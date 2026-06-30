4386d5a34b242e6fae731eb073fbf9dda52143cc
Dustin Swales <dustin.swales@noaa.gov>
2026-06-30 22:59:57 +0000

    Add timestep_init to physics group

--- physics/Interstitials/MPAS/MPAS_PBL_generic_post.F90
+++ physics/Interstitials/MPAS/MPAS_PBL_generic_post.F90
@@ -10,6 +10,23 @@ module MPAS_PBL_generic_post
 
 contains
 
+!! \htmlinclude MPAS_PBL_generic_post_timestep_init.html
+!!
+  subroutine MPAS_PBL_generic_post_timestep_init (errmsg, errflg)
+    use ccpp_wp,  only : kind_phys
+
+    implicit none
+    ! CCPP error handling variables
+    character(len=*), intent(out) :: errmsg
+    integer,          intent(out) :: errflg
+
+    ! Initialize CCPP error handling variables
+    errmsg = ''
+    errflg = 0
+
+    print*,'CCPP: Calling MPAS_PBL_generic_post_timestep_init()'
+  end subroutine MPAS_PBL_generic_post_timestep_init
+  
 ! #########################################################################################
 !> \brief This scheme sets up the vertically diffused tracer array for any PBL scheme based
 !! on the microphysics scheme chosen
@@ -31,7 +48,7 @@ contains
     errmsg = ''
     errflg = 0
 
-    print*,'CCPP: Calling MPAS_PBL_generic_pose_run()'
+    print*,'CCPP: Calling MPAS_PBL_generic_post_run()'
   end subroutine MPAS_PBL_generic_post_run
 
 end module MPAS_PBL_generic_post
